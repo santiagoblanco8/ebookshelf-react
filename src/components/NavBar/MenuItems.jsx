3d8d768887ff4bf0174b31751fed378905991081
Santiago Blanco <santiagoblanco88w@gmail.com>
2022-03-09 23:36:56 -0300

    Navbar con scroll completada casi

--- src/components/NavBar/MenuItems.jsx
+++ src/components/NavBar/MenuItems.jsx
@@ -20,7 +20,7 @@ export const MenuItems = [
         cName: 'nav-links'
     },
     {
-        title: 'Iniciar Sesión',
+        title: 'About Us',
         url: '#',
         cName: 'nav-links'
     },
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from "react"
 import { MenuItems } from "./MenuItems"
+import { Button } from "../Button"
 import './NavBar.css'
 
 class NavBar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">
                     eBookShelf <i className="fab fa-react"></i>
                 </h1>
-                <div className="menu-icon">
-
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
@@ -24,6 +31,7 @@ class NavBar extends Component {
                     })}
                     
                 </ul>
+                <Button>Iniciar Sesión</Button>
             </nav>    
         )               
     }
